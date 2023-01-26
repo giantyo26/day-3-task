bfa5b18775815963f386daf2de72ce85004bfcb8
giantyo <giantyoard@gmail.com>
2023-01-26 20:17:58 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function getData() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+
+    if(name == "") {
+        alert("Type you're name!")
+    } else if(email == "") {
+        alert("Input your email!")
+    } else if(phone == "") {
+        alert("Type your phone number!")
+    } else if(subject == "") {
+        alert("Choose the subject")
+    } else if(message == "") {
+        alert("Fill out the message")
+    } else {
+        const defaultEmail = "giantyoard@gmail.com";
+
+        let mailto = document.createElement("a");
+        mailto.href = `mailto: ${defaultEmail}?subject= ${subject}&body = Hello, my name is ${name},
+                      ${message}, you can contact me on this number:${phone}`;
+        mailto.click();
+    }
+
+}
